0831dee320da72e23795483e7bfd863d020b93bd
chyyran <ronny@ronnychan.ca>
2023-01-25 01:32:10 -0500

    d3d12: load lut texture data

--- librashader-runtime-d3d12/src/texture.rs
+++ librashader-runtime-d3d12/src/texture.rs
@@ -1,10 +1,11 @@
+use std::mem::ManuallyDrop;
 use crate::error;
 use crate::error::assume_d3d12_init;
 use crate::heap::{D3D12DescriptorHeap, D3D12DescriptorHeapSlot, LutTextureHeap};
-use crate::util::d3d12_get_closest_format;
+use crate::util::{d3d12_get_closest_format, d3d12_resource_transition, d3d12_update_subresources};
 use librashader_common::{FilterMode, ImageFormat, Size, WrapMode};
 use librashader_runtime::image::Image;
-use windows::Win32::Graphics::Direct3D12::{ID3D12CommandList, ID3D12Device, ID3D12GraphicsCommandList, ID3D12Resource, D3D12_CPU_PAGE_PROPERTY_UNKNOWN, D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING, D3D12_FEATURE_DATA_FORMAT_SUPPORT, D3D12_FORMAT_SUPPORT1_SHADER_SAMPLE, D3D12_FORMAT_SUPPORT1_TEXTURE2D, D3D12_HEAP_FLAG_NONE, D3D12_HEAP_PROPERTIES, D3D12_HEAP_TYPE_DEFAULT, D3D12_HEAP_TYPE_UPLOAD, D3D12_MEMORY_POOL_UNKNOWN, D3D12_PLACED_SUBRESOURCE_FOOTPRINT, D3D12_RESOURCE_DESC, D3D12_RESOURCE_DIMENSION_BUFFER, D3D12_RESOURCE_DIMENSION_TEXTURE2D, D3D12_RESOURCE_STATE_GENERIC_READ, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, D3D12_SHADER_RESOURCE_VIEW_DESC, D3D12_SHADER_RESOURCE_VIEW_DESC_0, D3D12_SRV_DIMENSION_TEXTURE2D, D3D12_TEX2D_SRV, D3D12_TEXTURE_LAYOUT_ROW_MAJOR, D3D12_RANGE};
+use windows::Win32::Graphics::Direct3D12::{ID3D12CommandList, ID3D12Device, ID3D12GraphicsCommandList, ID3D12Resource, D3D12_CPU_PAGE_PROPERTY_UNKNOWN, D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING, D3D12_FEATURE_DATA_FORMAT_SUPPORT, D3D12_FORMAT_SUPPORT1_SHADER_SAMPLE, D3D12_FORMAT_SUPPORT1_TEXTURE2D, D3D12_HEAP_FLAG_NONE, D3D12_HEAP_PROPERTIES, D3D12_HEAP_TYPE_DEFAULT, D3D12_HEAP_TYPE_UPLOAD, D3D12_MEMORY_POOL_UNKNOWN, D3D12_PLACED_SUBRESOURCE_FOOTPRINT, D3D12_RESOURCE_DESC, D3D12_RESOURCE_DIMENSION_BUFFER, D3D12_RESOURCE_DIMENSION_TEXTURE2D, D3D12_RESOURCE_STATE_GENERIC_READ, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, D3D12_SHADER_RESOURCE_VIEW_DESC, D3D12_SHADER_RESOURCE_VIEW_DESC_0, D3D12_SRV_DIMENSION_TEXTURE2D, D3D12_TEX2D_SRV, D3D12_TEXTURE_LAYOUT_ROW_MAJOR, D3D12_RANGE, D3D12_SUBRESOURCE_DATA, D3D12_RESOURCE_STATE_COPY_DEST};
 use windows::Win32::Graphics::Dxgi::Common::DXGI_SAMPLE_DESC;
 
 pub struct LutTexture {
@@ -24,7 +25,7 @@ impl LutTexture {
         filter: FilterMode,
         wrap_mode: WrapMode,
         mipmap: bool,
-    ) -> error::Result<LutTexture> {
+    ) -> error::Result<(LutTexture, ID3D12Resource)> {
         // todo: d3d12:800
         let mut desc = D3D12_RESOURCE_DESC {
             Dimension: D3D12_RESOURCE_DIMENSION_TEXTURE2D,
@@ -93,8 +94,8 @@ impl LutTexture {
         };
 
         let mut layout = D3D12_PLACED_SUBRESOURCE_FOOTPRINT::default();
-        let mut numrows = 0;
-        let mut rowsize = 0;
+        // let mut numrows = 0;
+        // let mut rowsize = 0;
         let mut total = 0;
         // texture upload
         unsafe {
@@ -104,8 +105,7 @@ impl LutTexture {
                 1,
                 0,
                 Some(&mut layout),
-                Some(&mut numrows),
-                Some(&mut rowsize),
+                None, None,
                 Some(&mut total),
             );
 
@@ -137,27 +137,28 @@ impl LutTexture {
         assume_d3d12_init!(upload, "CreateCommittedResource");
 
         unsafe {
-            let mut range = D3D12_RANGE {
-                Begin: 0,
-                End: 0
-            };
+            let subresource = [D3D12_SUBRESOURCE_DATA {
+                pData: source.bytes.as_ptr().cast(),
+                RowPitch: 4 * source.size.width as isize,
+                SlicePitch: (4 * source.size.width * source.size.height) as isize,
+            }];
 
-            let mut buf = std::ptr::null_mut();
+            d3d12_resource_transition(cmd, &resource, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, D3D12_RESOURCE_STATE_COPY_DEST);
 
-            upload.Map(0, Some(&range), Some(&mut buf))?;
+            d3d12_update_subresources(cmd, &resource,
+                                      &upload, 0, 0, 1, &subresource)?;
 
-            upload.Unmap(0, None);
-
+            d3d12_resource_transition(cmd, &resource, D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE)
         }
         // todo: upload image data to textur
 
-        Ok(LutTexture {
+        Ok((LutTexture {
             resource,
             descriptor,
             size: source.size,
             filter,
             wrap_mode,
-        })
+        }, upload))
     }
 }
 
