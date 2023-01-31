22599285d3805542534344a0c08110b4d2ececc1
chyyran <ronny@ronnychan.ca>
2023-01-30 23:30:11 -0500

    d3d12: load vertex buffers

--- librashader-runtime-d3d12/src/filter_pass.rs
+++ librashader-runtime-d3d12/src/filter_pass.rs
@@ -1,5 +1,17 @@
+use rustc_hash::FxHashMap;
+use librashader_presets::ShaderPassConfig;
+use librashader_reflect::reflect::semantics::{MemberOffset, UniformBinding};
+use librashader_reflect::reflect::ShaderReflection;
+use librashader_runtime::uniforms::UniformStorage;
+use crate::buffer::D3D12ConstantBuffer;
 use crate::graphics_pipeline::D3D12GraphicsPipeline;
 
 pub(crate) struct FilterPass {
-    pub(crate) pipeline: D3D12GraphicsPipeline
+    pub(crate) pipeline: D3D12GraphicsPipeline,
+    pub(crate) reflection: ShaderReflection,
+    pub(crate) config: ShaderPassConfig,
+    pub(crate) uniform_bindings: FxHashMap<UniformBinding, MemberOffset>,
+    pub uniform_storage: UniformStorage,
+    pub(crate) push_cbuffer: Option<D3D12ConstantBuffer>,
+    pub(crate) ubo_cbuffer: Option<D3D12ConstantBuffer>,
 }
