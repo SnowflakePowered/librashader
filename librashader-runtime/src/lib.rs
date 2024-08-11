3d5966c11cf125a1e8672b4184a683a7a6c29600
chyyran <ronny@ronnychan.ca>
2024-08-11 00:55:59 -0400

    fix: remove extern crate core in runtime

--- librashader-runtime/src/lib.rs
+++ librashader-runtime/src/lib.rs
@@ -7,8 +7,6 @@
 //! If you are _writing_ a librashader runtime implementation, using these traits and helpers will
 //! help in maintaining consistent behaviour in binding semantics and image handling.
 
-extern crate core;
-
 /// Scaling helpers.
 pub mod scaling;
 
