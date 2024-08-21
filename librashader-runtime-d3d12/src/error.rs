3094fbd0b1c67a92b5e7e887f9621e415d7cf89c
chyyran <ronny@ronnychan.ca>
2024-08-21 00:37:35 -0400

    rt: mark error types as non_exhaustive

--- librashader-runtime-d3d12/src/error.rs
+++ librashader-runtime-d3d12/src/error.rs
@@ -6,6 +6,7 @@ use thiserror::Error;
 
 /// Cumulative error type for Direct3D12 filter chains.
 #[derive(Error, Debug)]
+#[non_exhaustive]
 pub enum FilterChainError {
     #[error("invariant assumption about d3d12 did not hold. report this as an issue.")]
     Direct3DOperationError(&'static str),
