e5d426e1f7d63410c836072c2cafddf7d36c7cb6
chyyran <ronny@ronnychan.ca>
2025-04-11 00:32:03 -0400

    rt(gl): update glow

--- librashader-runtime-gl/src/gl/gl3/lut_load.rs
+++ librashader-runtime-gl/src/gl/gl3/lut_load.rs
@@ -58,7 +58,7 @@ impl LoadLut for Gl3LutLoad {
                     image.size.height as i32,
                     glow::RGBA,
                     glow::UNSIGNED_BYTE,
-                    PixelUnpackData::Slice(&image.bytes),
+                    PixelUnpackData::Slice(Some(&image.bytes)),
                 );
 
                 let mipmap = levels > 1;
