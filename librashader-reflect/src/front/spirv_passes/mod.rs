2e619d4d1c0f8d9fafb15ed4a4d984249968ccc8
chyyran <ronny@ronnychan.ca>
2026-05-24 01:54:18 +0900

    reflect(naga): split IO arrays because WGSL doesn't support array inouts

--- librashader-reflect/src/front/spirv_passes/mod.rs
+++ librashader-reflect/src/front/spirv_passes/mod.rs
@@ -2,6 +2,7 @@ pub mod harden_normalize;
 pub mod link_input_outputs;
 pub mod lower_loop_sample_lod;
 pub mod lower_samplers;
+pub mod split_io_arrays;
 
 // Load SPIR-V as an rspirv module
 pub(crate) fn load_module(words: &[u32]) -> rspirv::dr::Module {
